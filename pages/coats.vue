1cb0eaa69a7bd2304ef60707b27cadde93b089d6
Anthony Mooney <anthony@nixondesign.com>
2019-07-19 09:34:27 +0100

    initial commit

---
+++ pages/coats.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="clothrack">
+    <h1>Get a Coat Today!</h1>
+    <div class="clothes">
+    <p><nuxt-link to="/">Go back to the Home page</nuxt-link></p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .tweakOpacity-enter-active, .tweakOpacity-leave-active {
+      transition: opacity .40s ease-out;
+    }
+    .tweakOpacity-enter, .tweakOpacity-leave-active {
+      opacity: 0;
+    }
+    .clothrack {
+      padding: 50px;
+      width: 100vw;
+      height: 100vh;
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: center;
+    }
+    .clothes {
+      display: flex;
+      justify-content: space-evenly;
+      flex-wrap: wrap;
+      width: 500px;
+    }
+    html, body {
+      background: #9fdfbf;
+      color: #000000;
+      width: 100vw;
+      height: 100vh;
+    }
+    h1 {
+      width: 1000px;
+      justify-content: center;
+      display: flex;
+    }
+    a, a:visited {
+      color: #ffffff;
+      text-decoration: none;
+    }
+
+    </style>
---
+++ pages/index.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="clothrack">
+    <h1>Welcome</h1>
+    <div class="clothes">
+      <p ref="link"><nuxt-link to="/shirts">Shirts</nuxt-link></p>
+      <p><nuxt-link to="/trousers">Trousers</nuxt-link></p>
+      <p><nuxt-link to="/coats">Coats</nuxt-link></p>
+    </div>
+  </div>
+</template>
+
+<script>
+import {TimelineLite} from 'gsap'
+
+export default {
+  data() {
+    return {
+      tl: null
+    }
+  },
+
+  mounted() {
+    const tl = new TimelineLite();
+    const {link} = this.$refs
+
+    tl.to(link, 1, { rotation: 720, ease: Back.easeInOut, })
+
+    this.tl = new TimelineLite({
+      onComplete: () => tl.restart()
+    })
+  }
+}
+
+</script>
+
+<style>
+  /* .page-enter-active, .page-leave-active {
+    transition: all .30s ease-out;
+  }
+
+  .page-enter, .page-leave-active {
+    opacity: 0;
+    transform: 50% 50%;
+  } */
+
+  .clothrack {
+    padding: 50px;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: space-evenly;
+    flex-wrap: wrap;
+  }
+
+  .clothes {
+    display: flex;
+    justify-content: space-evenly;
+    flex-wrap: wrap;
+    width: 500px;
+  }
+
+  html, body {
+    background: #9fdfbf;
+    color: #000000;
+    width: 100vw;
+    height: 100vh;
+  }
+
+  h1 {
+    width: 1000px;
+    justify-content: center;
+    display: flex;
+  }
+
+  a, a:visited {
+    color: #ffffff;
+    text-decoration: none;
+  }
+
+</style>
